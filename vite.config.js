456436bce791aae2a79856f065fd671b36191a11
KIRA <118154322+shelby232@users.noreply.github.com>
2024-05-25 17:13:00 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/potatocoin/', // замените "имя-репозитория" на имя вашего репозитория
+  build: {
+    outDir: 'dist' // или 'build', если вы изменили директорию для сборки
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
